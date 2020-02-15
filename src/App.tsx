045a6d90381d59b0f767237fcbebefb9bb92c1a1
Justin Byun <klofseattle@hotmail.com>
2020-02-14 18:56:41 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+
+type FormElem = React.FormEvent<HTMLFormElement>;
+
+interface ITodo {
+  text: string;
+  complete: boolean;
+}
+
+export default function App() {
+  const [value, setValue] = useState<string>(""); // default value here
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const handleSubmit = (e: FormElem): void => {
+    e.preventDefault();
+    addTodo(value);
+    setValue("");
+  };
+
+  const addTodo = (text: string): void => {
+    const newTodos: ITodo[] = [...todos, { text, complete: false }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    newTodos[index].complete = !newTodos[index].complete;
+    setTodos(newTodos);
+  };
+
+  // console.log(todos);
+  return (
+    <>
+      <h1>Make To Do list with Typescript</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          required
+        />
+        <button type="submit">Add To do</button>
+      </form>
+      <section>
+        {todos.map((todo: ITodo, index: number) => (
+          <div key={index}>
+            <p>{todo.text}</p>
+            <button type="button" onClick={() => completeTodo(index)}>
+              {" "}
+              {todo.complete ? "Incomplete" : "Complete"}
+            </button>
+          </div>
+        ))}
+      </section>
+    </>
+  );
+}
+
+{
+  /* <div>
+  <div>{todo.text}</div>
+  <button type='button' onClick={() => completeTodo(index: number? 'Incomplete':'Complete')}>{todo.complete}</button>
+          ))}
+        </div > */
+}
